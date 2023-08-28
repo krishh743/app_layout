540e6f29b54cc851bc91b2b433e29fec94660ee7
krishna kumar kushwaha <krishnakushwaha743@gmail.com>
2023-08-28 22:41:10 +0530

    bugfixes

--- src/Pages/home-page/HomePage.jsx
+++ src/Pages/home-page/HomePage.jsx
@@ -1,51 +1,50 @@
-import React, {useState, useEffect} from "react";
-import TextField from "@mui/material/TextField";
-import Autocomplete from "@mui/material/Autocomplete";
+// import React, {useState, useEffect} from "react";
+// import TextField from "@mui/material/TextField";
+// import Autocomplete from "@mui/material/Autocomplete";
 
-function HomePage() {
-  const [userData, setUserData] = useState([]);
-  const [inputValue, setInputValue] = useState("");
-  const [suggestedNames, setSuggestedNames] = useState([]);
+// function HomePage() {
+//   const [userData, setUserData] = useState([]);
+//   const [inputValue, setInputValue] = useState("");
+//   const [suggestedNames, setSuggestedNames] = useState([]);
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((data) => setUserData(data))
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
+//   useEffect(() => {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((data) => setUserData(data))
+//       .catch((error) => console.error("Error fetching data:", error));
+//   }, []);
 
-  useEffect(() => {
-    const filteredNames = userData
-      .filter((user) =>
-        user.name.toLowerCase().includes(inputValue.toLowerCase())
-      )
-      .map((user) => user.name);
-    setSuggestedNames(filteredNames);
-  }, [inputValue, userData]);
+//   useEffect(() => {
+//     const filteredNames = userData
+//       .filter((user) =>
+//         user.name.toLowerCase().includes(inputValue.toLowerCase())
+//       )
+//       .map((user) => user.name);
+//     setSuggestedNames(filteredNames);
+//   }, [inputValue, userData]);
 
-  return (
-    <div>
-      <h1>Users from API:</h1>
-      <Autocomplete
-        sx={{width:"50%"}}
-        options={suggestedNames}
-        renderInput={(params) => (
-          <TextField {...params} label="Search for names" variant="outlined" />
-        )}
-      />
-      <ul>
-        {userData.map((user) => (
-          <li key={user.id}>
-            <h3>{user.name}</h3>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h1>Users from API:</h1>
+//       <Autocomplete
+//         sx={{width:"50%"}}
+//         options={suggestedNames}
+//         renderInput={(params) => (
+//           <TextField {...params} label="Search for names" variant="outlined" />
+//         )}
+//       />
+//       <ul>
+//         {userData.map((user) => (
+//           <li key={user.id}>
+//             <h3>{user.name}</h3>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
 
-export default HomePage;
-
+// export default HomePage;
 
 // import React, {useState, useEffect} from "react";
 
@@ -99,3 +98,86 @@ export default HomePage;
 // }
 
 // export default HomePage;
+
+import React, {useState, useEffect, useDeferredValue} from "react";
+import TextField from "@mui/material/TextField";
+
+function HomePage() {
+  const [userData, setUserData] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [suggestedNames, setSuggestedNames] = useState([]);
+  const [timeoutRef, setTimeoutRef] = useState(null);
+
+  const apiData = () => {
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+      .then((response) =>
+        response.json().then((response) => {
+          console.log(response, "sjchsdfjkds");
+          setSuggestedNames(response);
+        })
+      )
+      .then((data) => setUserData(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      apiData();
+      // setTimeoutRef(timeout);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [inputValue]);
+
+  // useEffect(() => {
+  //   if (inputValue.toLowerCase().includes("fr")) {
+  //     const filteredNames = userData
+  //       .filter((user) =>
+  //         user.name
+  //           .toLowerCase()
+  //           .startsWith(inputValue.toLowerCase().replace("from ", "").trim())
+  //       )
+  //       .map((user) => user.name);
+  //     setSuggestedNames(filteredNames);
+  //   } else {
+  //     setSuggestedNames([]);
+  //   }
+  // }, [inputValue, userData]);
+
+  const handleSuggestionClick = (user) => {
+    setInputValue(user.name);
+    setSuggestedNames([]);
+  };
+
+  // const clearSuggestions = () => {
+  //   setSuggestedNames([]);
+  // };
+
+  return (
+    <div>
+      <h1>Users from API:</h1>
+      <TextField
+        label="Search for names"
+        variant="outlined"
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
+        fullWidth
+        id="fullWidth"
+      />
+      {suggestedNames.length > 0 && (
+        <div className="suggestions">
+          {suggestedNames.map((user, index) => (
+            <div
+              key={index}
+              className="suggestion"
+              onClick={() => handleSuggestionClick(user)}
+            >
+              {user.name}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default HomePage;
